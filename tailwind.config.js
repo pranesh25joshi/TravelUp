e47268d79c26c886e0d235dec0e2691bae7809f5
Pranesh <pranesh25joshi@gmail.com>
2024-02-03 12:32:37 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      backgroundImage: {
+        heropattern:'url("/vedansh/hero-bg.jpg")',
+        bottompattern:'url("/vedansh/cta-bg.jpg")'
+      },
+      spacing:{
+        100: '40rem'
+      },
+      screens:{
+        gd: '930px'
+      }
+    },
+  },
+  plugins: [],
+}
